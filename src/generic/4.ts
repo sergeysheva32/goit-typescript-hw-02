57c0d0399b1b718bdc86a3eb867e799ec237cb02
sergeysheva32 <sergeysheva32@gmail.com>
2024-01-08 21:31:13 +0100

    FIRST COMMIT

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,25 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+interface Props {
+  title: string;
+}
+
+class Component<T extends Props> {
+  constructor(public props: T) {
 
   }
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<Props> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
 
-export {};
+//* Приклад використання:
+const pageProps: Props = { title: 'test, work 👍🏼' };
+const myPage = new Page(pageProps);
+myPage.pageInfo();
+
+export { };
